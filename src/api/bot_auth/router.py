07655e71759c8fd20949115880fb385ab3768dcd
IldarkoS <ildar.org.ru@gmail.com>
2025-06-14 18:39:02 +0300

    add auth for bots

---
+++ src/api/bot_auth/router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+from src.telegram.auth_service import AuthService
+
+router = APIRouter(prefix="", tags=["Auth Bots"])
+auth_service = AuthService()
+
+class AuthRequest(BaseModel):
+    phone: str
+    api_id: int
+    api_hash: str
+
+class CodeSubmit(BaseModel):
+    code: str
+    phone: str
+    api_id: int
+    api_hash: str
+
+class PasswordSubmit(BaseModel):
+    password: str
+    phone: str
+    api_id: int
+    api_hash: str
+
+@router.post("/auth/request_code/")
+async def request_code(data: AuthRequest):
+    try:
+        await auth_service.request_code(data.phone, data.api_id, data.api_hash)
+        return {"status": "code sent"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/auth/submit_code/")
+async def submit_code(data: CodeSubmit):
+    result = await auth_service.submit_code(data.phone, data.code, data.api_id, data.api_hash)
+    return {"status": result}
+
+@router.post("/auth/submit_password/")
+async def submit_code(data: PasswordSubmit):
+    result = await auth_service.submit_password(data.phone, data.password, data.api_id, data.api_hash)
+    return {"status": result}
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.api.bots.router import router as bot_router
 from src.api.tasks.router import router as task_router
+from src.api.bot_auth.router import router as bot_auth_router
 from src.logging import configure_logging, LogLevel
 
 configure_logging(LogLevel.DEBUG)
@@ -15,3 +16,4 @@ async def index():
 
 app.include_router(bot_router)
 app.include_router(task_router)
+app.include_router(bot_auth_router)
