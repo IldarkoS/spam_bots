a4c65d7bc32b327eb2646157becb84b83d9de0a2
IldarkoS <ildar.org.ru@gmail.com>
2025-06-14 10:52:12 +0300

    add logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-
 from src.api.router import router as bot_router
+from src.logging import configure_logging, LogLevel
 
+configure_logging(LogLevel.DEBUG)
 app = FastAPI()
 
 
